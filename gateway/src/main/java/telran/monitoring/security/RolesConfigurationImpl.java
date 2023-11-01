19f302764ea5eca86497084cfe677036d1ccece8
stanislav-revva <nivadrive1@gmail.com>
2023-11-01 16:25:14 +0200

    security + refactoring

---
+++ gateway/src/main/java/telran/monitoring/security/RolesConfigurationImpl.java
@@ -0,0 +1,18 @@
+package telran.monitoring.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import telran.spring.security.RolesConfiguration;
+
+@Configuration
+public class RolesConfigurationImpl implements RolesConfiguration {
+
+	@Override
+	public void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+
+	}
+
+}
